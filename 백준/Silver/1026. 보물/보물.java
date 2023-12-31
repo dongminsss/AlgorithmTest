b1d61b52680e9332b5062cadbcaef027713b83fa
dongmin Lee <116552419+dongminsss@users.noreply.github.com>
2023-12-31 14:35:31 +0900

    [Silver IV] Title: 보물, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.java
+++ 백준/Silver/1026. 보물/보물.java
@@ -3,27 +3,28 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main{
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int length = Integer.parseInt(br.readLine());
         int[] A = new int[length];
         int[] B = new int[length];
-        StringTokenizer st1 = new StringTokenizer(br.readLine());
+        int result = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
         StringTokenizer st2 = new StringTokenizer(br.readLine());
-        for (int i =  0; i < length; i++) {
-            A[i] = Integer.parseInt(st1.nextToken());
+        for (int i = 0; i < length; i++) {
+
+            A[i] = Integer.parseInt(st.nextToken());
             B[i] = Integer.parseInt(st2.nextToken());
         }
         Arrays.sort(A);
         Arrays.sort(B);
-        int j = length - 1;
-        int sum = 0;
+        int count = length-1;
         for (int i = 0; i < length; i++) {
-            sum += A[j] * B[i];
-            j--;
+            result += (A[i] * B[count--]);
         }
-        System.out.println(sum);
-    }
+        System.out.println(result);
 
+
+    }
 }
