3fd19e1b48462b0bf7e04e190f4a2b01ca384ff9
dongmin Lee <116552419+dongminsss@users.noreply.github.com>
2024-10-01 14:31:09 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 104 ms, Memory: 16796 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,54 +1,58 @@
-import java.io.*;
+
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int length;
-    static int[] numbers;
-    static int[] cals;
+    private static int length;
+    private static int[] numbers;
+    private static int[] cals;
     static long minNumber = Integer.MAX_VALUE;
     static long maxNumber = Integer.MIN_VALUE;
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        length = n;
         numbers = new int[n];
         cals = new int[4];
-        for (int i = 0; i < n; i++) {
+        length = n;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i<n; i++) {
             numbers[i] = Integer.parseInt(st.nextToken());
         }
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < 4; i++) {
+        for(int i = 0; i<4; i++) {
             cals[i] = Integer.parseInt(st.nextToken());
         }
-        cal(numbers[0], 1);
+        calculate(numbers[0], 1);
         System.out.println(maxNumber);
         System.out.println(minNumber);
     }
-    private static void cal(int n , int depth) {
+
+    private static void calculate(int number, int depth) {
         if(depth == length) {
-            maxNumber = Math.max(maxNumber, n);
-            minNumber = Math.min(minNumber, n);
+            maxNumber = Math.max(maxNumber, number);
+            minNumber = Math.min(minNumber, number);
             return;
         }
         for(int i = 0; i<4; i++) {
             if(cals[i] > 0) {
                 cals[i]--;
-
-                if(i == 0) {
-                    cal(n + numbers[depth], depth + 1);
-                } else if ( i == 1) {
-                    cal(n - numbers[depth], depth + 1);
-                } else if ( i == 2) {
-                    cal(n * numbers[depth], depth + 1);
-                } else if ( i == 3) {
-                    cal(n / numbers[depth], depth + 1);
+                if (i == 0) {
+                    calculate(number + numbers[depth], depth + 1);
+                } else if (i == 1) {
+                    calculate(number - numbers[depth], depth + 1);
+                } else if (i == 2) {
+                    calculate(number * numbers[depth], depth + 1);
+                } else {
+                    calculate(number / numbers[depth], depth + 1);
                 }
 
                 cals[i]++;
             }
         }
+
     }
+
 
 }
