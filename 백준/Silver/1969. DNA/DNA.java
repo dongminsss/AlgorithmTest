95b0d5b84180f276c420d4c66f69a720264d1b55
dongmin Lee <116552419+dongminsss@users.noreply.github.com>
2024-10-01 15:47:51 +0900

    [Silver IV] Title: DNA, Time: 112 ms, Memory: 14500 KB -BaekjoonHub

--- 백준/Silver/1969. DNA/DNA.java
+++ 백준/Silver/1969. DNA/DNA.java
@@ -1,5 +1,6 @@
-import java.io.*;
+
 import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -7,49 +8,61 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        char[][] charArr = new char[n][m];
+        String result = "";
+        int numberResult = 0;
+        char[][] matrix = new char[n][m];
         for (int i = 0; i < n; i++) {
-            String dna = br.readLine();
-            char[] charArray = dna.toCharArray();
-            charArr[i] = charArray;
+            String str = br.readLine();
+            matrix[i] = str.toCharArray();
         }
-        makeDna(charArr);
-    }
 
-    static void makeDna(char[][] arr) {
-        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < m; i++){
 
-        for (int i = 0; i < arr[0].length; i++) {
-            TreeMap<Character, Integer> map = new TreeMap<>();
-            for (int j = 0; j < arr.length; j++) {
-                char ch = arr[j][i];
-                map.put(ch, map.getOrDefault(ch, 0) + 1);
-            }
-            char mostChar = map.firstKey();
-            int maxCount = map.get(mostChar);
-            for (Map.Entry<Character, Integer> entry : map.entrySet()) {
-                if(entry.getValue() > maxCount) {
-                    mostChar = entry.getKey();
-                    maxCount = entry.getValue();
+            int[] counts = new int[4];
+            int targetIndex = 0;
+
+            for(int j = 0; j < n; j++){
+                char ch = matrix[j][i];
+                if(ch == 'A'){
+                    counts[0]++;
+                } else if(ch == 'C'){
+                    counts[1]++;
+                } else if(ch == 'G'){
+                    counts[2]++;
+                } else {
+                    counts[3]++;
                 }
             }
-            sb.append(mostChar);
-        }
-        int total = 0;
-        for (int i = 0; i < arr.length; i++) {
-            total += calHamingDistance(sb.toString(), new String(arr[i]));
-        }
-        System.out.println(sb);
-        System.out.println(total);
-    }
 
-    static int calHamingDistance(String s1, String s2) {
-        int distance = 0;
-        for (int i = 0; i < s1.length(); i++) {
-            if(s1.charAt(i) != s2.charAt(i)) {
-                distance++;
+            for(int j = 1; j < 4; j++){
+                if(counts[j] > counts[targetIndex]){
+                    targetIndex = j;
+                }
             }
+            for(int j = 0; j < 4; j++){
+                if(targetIndex == j) {
+                    continue;
+                } else {
+                    if(counts[j] > 0) numberResult += counts[j];
+                }
+            }
+
+            if(targetIndex == 0) {
+                result += "A";
+            } else if(targetIndex == 1) {
+                result += "C";
+            } else if(targetIndex == 2) {
+                result += "G";
+            } else {
+                result += "T";
+            }
+
+
+
         }
-        return distance;
+
+        System.out.println(result);
+        System.out.println(numberResult);
+
     }
 }
