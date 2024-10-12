81dfd7084843daf741c7cae51d3f369ccb0b9267
dongmin Lee <116552419+dongminsss@users.noreply.github.com>
2024-10-12 15:35:06 +0900

    [Gold V] Title: A와 B, Time: 116 ms, Memory: 14380 KB -BaekjoonHub

---
+++ 백준/Gold/12904. A와 B/A와 B.java
@@ -0,0 +1,27 @@
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        String t = br.readLine();
+        StringBuilder sb = new StringBuilder(t);
+
+        while(sb.length() > s.length()) {
+            if(sb.charAt(sb.length()-1) == 'A') {
+                sb.deleteCharAt(sb.length()-1);
+            } else {
+                sb.deleteCharAt(sb.length()-1);
+                sb.reverse();
+            }
+        }
+
+        if(sb.toString().equals(s)) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
