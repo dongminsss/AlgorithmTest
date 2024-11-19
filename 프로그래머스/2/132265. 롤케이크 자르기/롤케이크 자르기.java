be5877df549435cc5ba7a6ecc66311d686b4295e
dongmin Lee <116552419+dongminsss@users.noreply.github.com>
2024-11-19 17:56:13 +0900

    [level 2] Title: 롤케이크 자르기, Time: 263.44 ms, Memory: 183 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -1,6 +1,26 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] topping) {
-        int answer = -1;
+        Map<Integer, Integer> ch = new HashMap<>();
+        Map<Integer, Integer> sh = new HashMap<>();
+        int answer = 0;
+        for(int i : topping) {
+            sh.put(i, sh.getOrDefault(i, 0) + 1);
+        }
+        
+        for(int i : topping) {
+            sh.put(i, sh.get(i) - 1);
+            if(sh.get(i) == 0) sh.remove(i);
+            ch.put(i, ch.getOrDefault(i, 0) + 1);
+            
+            if(sh.size() == ch.size()) answer++;
+        }
+        
+        
+        
+        
         return answer;
     }
+    
 }
