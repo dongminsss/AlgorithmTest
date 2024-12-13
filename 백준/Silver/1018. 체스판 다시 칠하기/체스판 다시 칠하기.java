103169d4778c022ccb306b3c3c1ed6bd29b375f5
dongmin Lee <116552419+dongminsss@users.noreply.github.com>
2024-12-13 20:38:35 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 112 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -3,57 +3,55 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        char[][] matrix = new char[n][m];
-        for (int i = 0; i < n; i++) {
-            matrix[i] = br.readLine().toCharArray();
+        char[][] board = new char[n][m];
+
+        for(int i = 0; i < n; i++) {
+            char[] line = br.readLine().toCharArray();
+            board[i] = line;
         }
-        int minCount = Integer.MAX_VALUE;
+        int answer = Integer.MAX_VALUE;
+
+        char[][] whiteStartBoard = new char[8][8];
+        char[][] blackStartBoard = new char[8][8];
+        for(int i = 0; i < 8; i++) {
+            for(int j = 0; j < 8; j++) {
+                if(i % 2 == 0 && j % 2 == 0) {
+                    whiteStartBoard[i][j] = 'W';
+                    blackStartBoard[i][j] = 'B';
+                } else if(i % 2 == 0 && j % 2 == 1) {
+                    whiteStartBoard[i][j] = 'B';
+                    blackStartBoard[i][j] = 'W';
+                } else if(i % 2 == 1 && j % 2 == 0) {
+                    whiteStartBoard[i][j] = 'B';
+                    blackStartBoard[i][j] = 'W';
+                } else {
+                    whiteStartBoard[i][j] = 'W';
+                    blackStartBoard[i][j] = 'B';
+                }
+            }
+        }
+
         for(int i = 0; i <= n - 8; i++) {
             for(int j = 0; j <= m - 8; j++) {
-                minCount = Math.min(minCount, isValid(matrix, i, j));
+                int count1 = 0;
+                int count2 = 0;
+                for(int k = i; k < 8 + i; k++) {
+                    for(int l = j; l < 8 + j; l++) {
+                        if(board[k][l] != whiteStartBoard[k - i][l - j]) count1++;
+                        if(board[k][l] != blackStartBoard[k - i][l - j]) count2++;
+                    }
+                }
+                answer = Math.min(answer, count1);
+                answer = Math.min(answer, count2);
+
             }
         }
-        System.out.println(minCount);
+        System.out.println(answer);
 
-
-    }
-
-    private static int isValid(char[][] charArr, int row, int col) {
-
-        int count1 = 0;
-        int count2 = 0;
-        char[][] validMatrix = {
-                {'B','W','B','W','B','W','B','W'},
-                {'W','B','W','B','W','B','W','B'},
-                {'B','W','B','W','B','W','B','W'},
-                {'W','B','W','B','W','B','W','B'},
-                {'B','W','B','W','B','W','B','W'},
-                {'W','B','W','B','W','B','W','B'},
-                {'B','W','B','W','B','W','B','W'},
-                {'W','B','W','B','W','B','W','B'}};
-
-        char[][] validMatrix2 = {
-                {'W','B','W','B','W','B','W','B'},
-                {'B','W','B','W','B','W','B','W'},
-                {'W','B','W','B','W','B','W','B'},
-                {'B','W','B','W','B','W','B','W'},
-                {'W','B','W','B','W','B','W','B'},
-                {'B','W','B','W','B','W','B','W'},
-                {'W','B','W','B','W','B','W','B'},
-                {'B','W','B','W','B','W','B','W'}};
-        for(int i = 0; i< 8; i++) {
-            for(int j = 0; j < 8; j++) {
-                if(charArr[row + i][col + j] != validMatrix[i][j]) count1++;
-                if(charArr[row + i][col + j] != validMatrix2[i][j]) count2++;
-            }
-        }
-
-        return Math.min(count1, count2);
     }
 }
