8f4a5dc53cc95858a77bb50a6ffdfc4f158f59f1
dongmin Lee <116552419+dongminsss@users.noreply.github.com>
2024-12-16 16:14:12 +0900

    [Silver III] Title: 소수 구하기, Time: 696 ms, Memory: 31652 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,25 +1,30 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int start = Integer.parseInt(st.nextToken());
-        int end = Integer.parseInt(st.nextToken());
-        for(int i = start; i<=end; i++) {
-            if(primeNumber(i)) System.out.println(i);
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        boolean[] primeNumber = new boolean[m + 1];
+        primeNumber[0] = primeNumber[1] = true;
+
+        for(int i = 2; i<= Math.sqrt(m); i++) {
+            if(!primeNumber[i]) {
+                for(int j = i + i; j <=m; j += i) {
+                    primeNumber[j] = true;
+                }
+            }
+
         }
 
-    }
-    public static boolean primeNumber(int number) {
-        if(number == 0 || number == 1 ) return false;
-        for(int i = 2; i<=Math.sqrt(number); i++) {
-            if(number % i == 0) return false;
+        for(int i = n; i<=m; i++) {
+            if(!primeNumber[i]) System.out.println(i);
         }
-        return true;
+
+
     }
 }
