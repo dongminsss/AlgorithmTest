be5a876d49f5616cd69ef899cbacdd5765e9795a
dongmin Lee <116552419+dongminsss@users.noreply.github.com>
2024-12-25 14:02:30 +0900

    [Silver IV] Title: 카드2, Time: 164 ms, Memory: 29096 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,24 +1,25 @@
-import java.io.*;
+
 import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        ArrayDeque<Integer> deque = new ArrayDeque<>();
-        for (int i = 1; i <= n; i++) {
-            deque.add(i);
+        Queue<Integer> queue = new ArrayDeque<>();
+        for(int i = 1; i <= n; i++) {
+            queue.offer(i);
         }
-        int index = 1;
-
-        while(deque.size() > 1) {
-            if(index % 2 == 0) {
-                deque.offer(deque.poll());
-            } else {
-                deque.poll();
+        if(queue.size() == 1) System.out.println(queue.poll());
+        while (queue.size() > 1) {
+            queue.poll();
+            queue.offer(queue.poll());
+            if(queue.size() == 1) {
+                System.out.println(queue.poll());
             }
-            index++;
         }
-        System.out.println(deque.poll());
+
+
+
     }
 }
