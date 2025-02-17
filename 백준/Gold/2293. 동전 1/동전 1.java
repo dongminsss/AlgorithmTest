84935e0b50fb495c424f7a2f30cc80697e36f092
dongmin Lee <116552419+dongminsss@users.noreply.github.com>
2025-02-17 23:48:29 +0900

    [Gold IV] Title: 동전 1, Time: 112 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -1,31 +1,31 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-public class Main {
-
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-        int[] coins = new int[n];
-        int[] dp = new int[k + 1];
 
-        for(int i = 0; i < n; i++) {
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] dp = new int[m + 1];
+        int[] coins = new int[n];
+        for (int i = 0; i < n; i++) {
             coins[i] = Integer.parseInt(br.readLine());
         }
 
-        Arrays.sort(coins);
         dp[0] = 1;
-
-        for(int coin : coins) {
-            for(int i = coin; i<= k; i++) {
+        for (int coin : coins) {
+            for (int i = coin; i <= m; i++) {
                 dp[i] += dp[i - coin];
             }
         }
-        
-        System.out.println(dp[k]);
+        System.out.println(dp[m]);
 
 
-     }
+
+    }
 }
