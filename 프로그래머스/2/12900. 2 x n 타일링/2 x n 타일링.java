6e44d724a08a40ca3468c0687bc0978034d514ec
dongmin Lee <116552419+dongminsss@users.noreply.github.com>
2025-09-19 11:22:43 +0900

    [level 2] Title: 2 x n 타일링, Time: 1.74 ms, Memory: 53.8 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -1,16 +1,18 @@
+import java.util.*;
+
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        if(n == 1) answer = 1;
-        if(n == 2) answer = 2;
-        int prevPrevNumber = 1;
-        int prevNumber = 2;
-        for(int i = 2; i<n; i++) {
-            answer = ( prevPrevNumber + prevNumber ) % 1000000007;
-            prevPrevNumber = prevNumber % 1000000007;
-            prevNumber = answer;
+        if (n == 1) answer = 1;
+        if (n > 1) answer = 2;
+        int prevNumber = 1;
+        for(int i = 3; i<=n; i++) {
+            int temp = answer;
+            answer = ( answer + prevNumber ) % 1000000007;
+            prevNumber = temp;
         }
         
-        return answer;
+        
+        return answer % 1000000007;
     }
 }
