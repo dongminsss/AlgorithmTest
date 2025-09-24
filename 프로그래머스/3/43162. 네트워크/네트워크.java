46654e26269e5989b36824a9d07efcba210915dd
dongmin Lee <116552419+dongminsss@users.noreply.github.com>
2025-09-24 13:59:44 +0900

    [level 3] Title: 네트워크, Time: 0.91 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,30 +1,38 @@
 import java.util.*;
+
 class Solution {
-    static boolean[] visited;
-    static int[][] network;
-    static int count;
+    static ArrayList<ArrayList<Integer>> network = new ArrayList<>();
+    static boolean[] v;
+    
     public int solution(int n, int[][] computers) {
-        visited = new boolean[n];
-        network = computers;
-        for(int i = 0; i < n; i++) {
-            if(!visited[i]) {
-                dfs(i);
-                count++;
-            }   
+        v = new boolean[n];
+        
+        for(int i = 0; i<n; i++) {
+            network.add(new ArrayList<>());
+        }    
+        for(int i = 0; i<n; i++) {
+            for(int j = 0; j<n; j++) {
+                if(i != j && computers[i][j] == 1) {
+                    network.get(i).add(j);
+                    network.get(j).add(i);
+                }
+            }
         }
-        int answer = count;
+        
+        int answer = 0;
+        
+        for(int i = 0; i<n; i++) {
+            answer += dfs(i);
+        }
         return answer;
     }
     
-    private static void dfs(int computer) {
-        if(visited[computer]) return;
-        
-        visited[computer] = true;
-        
-        for(int i = 0; i < network[computer].length; i++) {
-            if(network[computer][i] != 0 && computer != i) dfs( i);
+    private int dfs(int idx) {
+        if(v[idx]) return 0;
+        v[idx] = true;
+        for(int computer : network.get(idx)) {
+            dfs(computer);
         }
-        
-        
+        return 1;
     }
 }
