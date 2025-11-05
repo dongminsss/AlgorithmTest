545350663be2fe5134b6938b0e965a8b6e2e1f46
dongmin Lee <116552419+dongminsss@users.noreply.github.com>
2025-11-05 10:43:32 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 13.80 ms, Memory: 80.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        int[] arr = Arrays.stream(s.split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(arr);
+        String answer = arr[0] + " " + arr[arr.length-1];
+        return answer;
+    }
+}
