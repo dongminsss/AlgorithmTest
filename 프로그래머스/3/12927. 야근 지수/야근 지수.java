1b1059328d40b56dec6ac8bd20c91173628d529c
dongmin Lee <116552419+dongminsss@users.noreply.github.com>
2025-11-07 15:07:08 +0900

    [level 3] Title: 야근 지수, Time: 133.07 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -4,23 +4,18 @@ class Solution {
     public long solution(int n, int[] works) {
         long answer = 0;
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int a : works) pq.add(a);
         
-        for(int i : works) {
-            pq.add(i);
+        while(n > 0) {
+            int hour = pq.poll();
+            if(hour == 0) break;
+            n--;
+            pq.add(hour-1);
         }
-        for(int i = 0; i<n; i++) {
-            if(!pq.isEmpty()){
-                int number = pq.poll();
-                if(number > 0) {
-                    pq.add(--number);
-                }    
-            }
-            
+        for(int i : pq) {
+            answer += i * i;
         }
-        while(!pq.isEmpty()) {
-            int number = pq.poll();
-            answer += number * number;
-        }
+        
         return answer;
     }
 }
