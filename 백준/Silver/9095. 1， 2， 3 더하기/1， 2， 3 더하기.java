332f64fbe999a71af773944fdaea65e19a0fd7ec
dongmin Lee <116552419+dongminsss@users.noreply.github.com>
2026-03-23 21:07:37 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 104 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,28 +1,29 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] answer = new int[n];
-        for(int i = 0; i < n; i++) {
-            int m = Integer.parseInt(br.readLine());
-            int[] dp = new int[m + 1];
-            if( m == 1) dp[1] = 1;
-            if( m == 2) dp[2] = 2;
-            if( m == 3) dp[3] = 4;
-            if(m > 3) {
-                dp[1] = 1;
-                dp[2] = 2;
-                dp[3] = 4;
-                for(int j = 4; j <= m; j++) {
-                    dp[j] = dp[j - 1] + dp[j - 2] + dp[j - 3];
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < n; i++){
+            int s =  Integer.parseInt(br.readLine());
+            if(s == 1) sb.append(1);
+            if(s == 2) sb.append(2);
+            if(s == 3) sb.append(4);
+            if(s > 3) {
+                int[] dp = new int[s];
+                dp[0] = 1;
+                dp[1] = 2;
+                dp[2] = 4;
+                for(int j = 3; j < dp.length; j++){
+                    dp[j] = dp[j-1] + dp[j-2] + dp[j-3];
                 }
+                sb.append(dp[s-1]);
             }
-            answer[i] = dp[m];
+            sb.append("\n");
         }
-        for(int a : answer) System.out.println(a);
-     }
+        System.out.println(sb.deleteCharAt(sb.length()-1));
+    }
 }
