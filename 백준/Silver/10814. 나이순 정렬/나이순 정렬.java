b0e62a564ed2492f44b70a7385df2789ccf42d1c
dongmin Lee <116552419+dongminsss@users.noreply.github.com>
2026-03-25 13:00:39 +0900

    [Silver V] Title: 나이순 정렬, Time: 1252 ms, Memory: 57408 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -2,45 +2,35 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.StringTokenizer;
 
-class Main {
-    static class Person implements Comparable<Person> {
-        int age;
-        String name;
-        int order;
-
-        Person(int age, String name, int order) {
-            this.age = age;
-            this.name = name;
-            this.order = order;
-        }
-        @Override
-        public int compareTo(Person o) {
-            if(this.age == o.age) {
-                return Integer.compare(this.order, o.order);
-            }
-            return Integer.compare(this.age, o.age);
-        }
-    }
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        Person[] people = new Person[n];
-        for(int i = 0; i < n; i++) {
+        int N = Integer.parseInt(br.readLine());
+        List<member> list = new ArrayList<>();
+        for(int i = 0; i < N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int age = Integer.parseInt(st.nextToken());
+            int old =  Integer.parseInt(st.nextToken());
             String name = st.nextToken();
-            people[i] = new Person(age, name, 1);
+            list.add(new member(old, name));
         }
-        Arrays.sort(people);
-        for(Person p : people) {
-            System.out.println(p.age + " " + p.name);
-        }
-
-
+        list.sort(Comparator.comparingInt(m -> m.old));
+        list.stream().forEach(System.out::println);
     }
 
+    static class member {
+        int old;
+        String name;
+        public member(int old, String name) {
+            this.old = old;
+            this.name = name;
+        }
+        @Override
+        public String toString() {
+            return old + " " + name;
+        }
+    }
 }
