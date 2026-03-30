42c20c3a8afa4a60f41125c6bb992fb644ab02f8
dongmin Lee <116552419+dongminsss@users.noreply.github.com>
2026-03-30 15:30:09 +0900

    [level 2] Title: 올바른 괄호, Time: 15.23 ms, Memory: 54.7 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/README.md
+++ 프로그래머스/2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
 
 ### 성능 요약
 
-메모리: 54.9 MB, 시간: 15.56 ms
+메모리: 54.7 MB, 시간: 15.23 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 16일 14:43:34
+2026년 03월 30일 15:30:06
 
 ### 문제 설명
 
--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,20 +2,18 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        if(s.charAt(0) == ')') return false;
-        
-        ArrayDeque<Character> deque = new ArrayDeque<>();
-         boolean answer = true;
-        for(char c : s.toCharArray()) {
-            if(c == ')' && !deque.isEmpty()) {
-                char ch = deque.pop();
-                if(ch != '(') answer = false;
-            } else {
-                deque.add(c);
-            }
-        }
-        if(!deque.isEmpty()) answer = false;    
+        boolean answer = true;
 
-        return answer;
+        ArrayDeque<Character> dq = new ArrayDeque<>();
+        dq.push(s.charAt(0));
+        for(int i = 1; i<s.length(); i++) {
+            char c = s.charAt(i);
+            if(!dq.isEmpty() && c == ')' && dq.peek() == '(') {
+                dq.pop();
+            } 
+            if(c == '(') dq.push(c);
+        }
+        
+        return dq.isEmpty();
     }
 }
