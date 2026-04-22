8b84eeccb08c5d3ac9d2ea5355a51974fff52108
dongmin Lee <116552419+dongminsss@users.noreply.github.com>
2026-04-22 19:21:14 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.14 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,16 +1,15 @@
-import java.util.*;
-
 class Solution {
     public long solution(int n) {
         long[] dp = new long[n+1];
         if(n == 1) return 1;
         if(n == 2) return 2;
-        dp[1] = 1;
-        dp[2] = 2;
-        for(int i = 3; i<=n; i++) {
-            dp[i] = (dp[i-2] + dp[i-1]) % 1234567;
+        if(n > 2) {
+            dp[1] = 1;
+            dp[2] = 2;
         }
-        
-        return dp[n] % 1234567;
+        for(int i = 3; i<=n; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
+        }
+        return dp[n];
     }
 }
