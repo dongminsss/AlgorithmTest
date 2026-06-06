6223b17529499253e73bcd9e5ad81547fa38abda
dongmin Lee <116552419+dongminsss@users.noreply.github.com>
2026-06-06 11:24:19 +0900

    [level 2] Title: 과제 진행하기, Time: 6.05 ms, Memory: 70 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.java
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.java
@@ -1,58 +1,53 @@
 import java.util.*;
+
 class Solution {
     public String[] solution(String[][] plans) {
         String[] answer = new String[plans.length];
         ArrayDeque<String[]> dq = new ArrayDeque<>();
         int idx = 0;
-        for(String[] plan : plans) {
-            plan[1] = replaceMinute(plan[1]);
+        for(String[] p : plans) {
+            p[1] = String.valueOf(replaceMinute(p[1]));
         }
-        Arrays.sort(plans, (o1, o2) -> Integer.parseInt(o1[1]) - Integer.parseInt(o2[1]));
-        // System.out.println(Arrays.deepToString(plans));
+        Arrays.sort(plans, (o1,o2) -> Integer.parseInt(o1[1]) - Integer.parseInt(o2[1]));
         
         for(int i = 0; i<plans.length-1; i++) {
             String name = plans[i][0];
-            int startTime = Integer.parseInt(plans[i][1]);
-            int playable = Integer.parseInt(plans[i][2]);
+            int start = Integer.parseInt(plans[i][1]);
+            int playTime = Integer.parseInt(plans[i][2]);
             int nextStart = Integer.parseInt(plans[i+1][1]);
+            int available = nextStart - start;
             
-            int available = nextStart - startTime;
-            
-            if(available >= playable) {
+            if(available >= playTime) {
                 answer[idx++] = name;
-                int remain = available - playable;
+                int remain = available - playTime;
                 
-                while(!dq.isEmpty() && remain >0) {
+                while(!dq.isEmpty() && remain > 0) {
                     String[] paused = dq.pop();
                     int pausedTime = Integer.parseInt(paused[1]);
                     if(pausedTime <= remain) {
-                        answer[idx++] = paused[0];
                         remain -= pausedTime;
+                        answer[idx++] = paused[0];
                     } else {
-                        paused[1] = String.valueOf(pausedTime - remain);
-                        dq.push(paused);
+                        dq.push(new String[]{paused[0], String.valueOf(pausedTime - remain)});
                         remain = 0;
                     }
                 }
             } else {
-                int remain = playable - available;
-                dq.push(new String[]{name, String.valueOf(remain)});
+                dq.push(new String[]{name, String.valueOf(playTime - available)});
             }
         }
+        
         answer[idx++] = plans[plans.length-1][0];
         
         while(!dq.isEmpty()) {
             answer[idx++] = dq.pop()[0];
         }
+        
         return answer;
     }
     
-    String replaceMinute(String s) {
-        int count = 0;
-        String[] arr = s.split(":");
-        count += Integer.parseInt(arr[0]) * 60 + Integer.parseInt(arr[1]);
-        return String.valueOf(count);
+    int replaceMinute(String str) {
+        String[] arr = str.split(":");
+        return Integer.parseInt(arr[0]) * 60 + Integer.parseInt(arr[1]);
     }
-    
-    
 }
